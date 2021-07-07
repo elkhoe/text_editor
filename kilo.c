e20c910e37f17b05f946219c83e18c60a1178ab1
elkhoe <ekhoe.bg@gmail.com>
2021-07-07 01:17:49 -0700

    more controls

--- kilo.c
+++ kilo.c
@@ -18,6 +18,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[2H", 3);
     perror(s);
     exit(1);
 }
@@ -53,6 +55,24 @@ char editorReadKey() {
         }
     return c;
 }
+/*** output ***/
+
+void editorDrawRows() {
+    int y;
+    for(y = 0; y < 24; y++) {
+        write(STDOUT_FILENO, "\r\n", 3);
+        }
+    }
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 
 /*** input ***/
 
@@ -61,6 +81,8 @@ void editorProcessKeypress() {
 
     switch(c) {
         case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
         }
@@ -72,6 +94,7 @@ int main() {
     enableRawMode();
 
     while(1) {
+        editorRefreshScreen();
         editorProcessKeypress();
         }
     return 0; 
